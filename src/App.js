a9286f313f74154565f73ce3293be9dd817cd5a1
LucasSVA <lucas.svanadze@gmail.com>
2022-07-22 10:54:41 +0200

    tailtwing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Form from './components/Form';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>hello</h1>
+        <h1 className="text-green-700">hello world</h1>
+        <Form />
       </div>
     );
   }
